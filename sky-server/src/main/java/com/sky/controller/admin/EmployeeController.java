f03247bed5abe626e6e66cd215c7d81c253ceff2
John Doe <1929897318@qq.com>
2026-05-23 16:42:16 +0800

    修改员工信息业务代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -95,4 +95,18 @@ public class EmployeeController {
         return Result.success();
     }
 
+    @GetMapping("{id}")
+    public Result<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息");
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @PutMapping
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("员工信息：{}", employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
+
 }
