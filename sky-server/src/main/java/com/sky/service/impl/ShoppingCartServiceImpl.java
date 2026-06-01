5d2df6a68d3e5facf9a07e713df60f2ad63cdc84
John Doe <1929897318@qq.com>
2026-06-01 16:31:46 +0800

    查看购物车业务代码

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -68,4 +68,13 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCartMapper.insert(shoppingCart);
         }
     }
+
+    // 查看购物车
+    @Override
+    public List<ShoppingCart> list() {
+        return shoppingCartMapper.list(ShoppingCart.builder()
+                .userId(BaseContext.getCurrentId())
+                .build()
+        );
+    }
 }
